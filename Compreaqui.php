0e8f3d411821eecca9aacdea6c12b90b052b2984
joao1lucca <joaoluccaangelo060@gmail.com>
2025-11-05 19:18:02 -0300

    Create Compreaqui.php

---
+++ Compreaqui.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    *{
+            margin: 0;
+            padding: 0;
+            font-family: Arial, Helvetica, sans-serif;
+    }
+    .botaovoltar {
+        margin-top: 20px;
+        margin-left: 7px;
+        display: inline-block;
+        background-color: #333333ff;
+        color: white;
+        padding: 8px 15px;
+        text-decoration: none;
+        border: none;
+        cursor: pointer;
+    }
+    h2{
+        margin-top: 2rem;
+        margin-left: 7px;
+    }
+    p{
+        margin-left: 7px;
+    }
+    </style>
+</head>
+<body>
+    <h2>Infelizmente nosso site está em manutenção.</h2>
+    <p>Entre em cotato ou dirija-se a uma de nossas lojas fisicas.</p>
+    <a href="index.php?pg=conteudo" class="botaovoltar">Voltar</a>
+</body>
+</html>
