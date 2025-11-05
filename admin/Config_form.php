bff48ec4a75da68a2d01b63b3c619d1e69b3a6ea
joao1lucca <joaoluccaangelo060@gmail.com>
2025-11-05 19:46:58 -0300

    Create Config_form.php

---
+++ admin/Config_form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Cadastro de cliente</h2>
+    <form action="?pg=clientes_cadastro" method="post">
+        <label>Nome:</label>
+        <input type="text" name="cliente"><br>
+        <label>cidade:</label>
+        <input type="text" name="cidade"><br> 
+        <label>Estado:</label>
+        <input type="text" name="estado"><br>
+        <input type="submit" value="Cadastrar cliente"> 
+         
+    </form>
+</body>
+</html>
