a4b1b8793bf5bcf8014481116223b032d95a7325
joao1lucca <joaoluccaangelo060@gmail.com>
2025-11-05 19:13:37 -0300

    Create Menu.php

---
+++ Menu.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+    <div class="container-fluid">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="?pg=conteudo">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pg=quemsomos">Quem Somos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pg=clientes">Clientes</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pg=faleconosco">Fale Conosco</a>
+            </li>
+        </ul>
+    </div>
+</nav>
