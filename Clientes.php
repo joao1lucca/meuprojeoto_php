4c1d9693ba5b9bcee6f26cd6a0db72cbc455223a
joao1lucca <joaoluccaangelo060@gmail.com>
2025-11-05 19:19:08 -0300

    Create Clientes.php

---
+++ Clientes.php
@@ -0,0 +1,51 @@
+<style>
+    *{
+            margin: 0;
+            padding: 0;
+            font-family: Arial, Helvetica, sans-serif;
+    }
+    div{
+        margin-left: 7px;
+    }
+    h2{
+        margin-left: 7px;
+    }
+</style>
+
+
+<?php
+    require_once "admin/config.inc.php";
+    $sql = "SELECT * FROM clientes";
+    $resultado = mysqli_query($conexao, $sql);
+    if(mysqli_num_rows($resultado) > 0) {
+    echo "<h2>Clientes</h2>";
+?>
+<div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Cidade</th>
+                <th>Estado</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while($dados = mysqli_fetch_array($resultado)){
+            ?>
+            <tr>
+                <td><?=$dados['cliente']?></td>
+                <td><?= $dados['cidade']?></td>
+                <td><?= $dados['estado']?></td>
+            </tr>
+            <?php
+                }
+            ?>
+        </tbody>
+    </table>
+</div>
+<?php
+    }else{
+        echo "<h2>Nenhum cliente escontrado</h2";
+    }
+?>
