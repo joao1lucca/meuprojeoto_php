a34f8084e38b9d01719a5e7ff23d86421f658980
joao1lucca <joaoluccaangelo060@gmail.com>
2025-11-05 19:44:17 -0300

    Create Config.inc.php

---
+++ admin/Config.inc.php
@@ -0,0 +1,11 @@
+<?php
+    $conexao = mysqli_connect("localhost", "root", "");
+    
+    $dg = mysqli_select_db($conexao, "projeto11");
+
+    if(!$conexao){
+        echo"<h2>Erro ao conectar com o banco de dados</h2>";
+    }
+
+
+    
