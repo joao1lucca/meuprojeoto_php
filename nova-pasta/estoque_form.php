d8c33938559440ba256546d9ecc73a6ef9d2a7ec
joao1lucca <joaoluccaangelo060@gmail.com>
2025-11-05 19:37:01 -0300

    Create estoque_form.php

---
+++ nova-pasta/estoque_form.php
@@ -0,0 +1,10 @@
+<h2>Adicione itens ao Estoque</h2>
+    
+<form action="?pg=estoque_cadastro" method="post">
+        <label>Item:</label>
+        <input type="text" name="item"><br>
+        <label>Quantidade:</label>
+        <input type="number" name="quantidade"><br> 
+        <label>Preço:</label>
+        <input type="number" name="preco"><br>
+        <input type="submit" value="Adicionar ao estoque"> 
